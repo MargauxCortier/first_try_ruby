47a39f9511daf993755295847c3d60bec4bb1c54
margauxCortier <cortier.margaux@gmail.com>
2018-09-19 17:57:03 +0200

    exo 9 done

---
+++ exo_09.rb
@@ -0,0 +1,12 @@
+puts "hello hello, c'est quoi ton nom ?"
+print "> "
+user_first_name = gets.chomp
+
+puts "et ton nom de famille ?"
+print "> "
+user_last_name = gets.chomp
+
+print "Bonjour "
+print user_first_name
+print " "
+puts user_last_name
