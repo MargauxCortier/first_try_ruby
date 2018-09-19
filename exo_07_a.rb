81ffcec1003c1e2a993d47ec4b2e5c10ddd0d09a
margauxCortier <cortier.margaux@gmail.com>
2018-09-19 17:48:39 +0200

    exo8 done

--- exo_07_a.rb
+++ exo_07_a.rb
@@ -1,3 +1,5 @@
 puts "Bonjour, c'est quoi ton blase ?"
 user_name = gets.chomp
 puts user_name
+
+#gets.chomp laisse case blanche pour que l'utilisateur rentre son nom
---
+++ exo_08.rb
@@ -0,0 +1,6 @@
+puts "what's up ? c'est quoi ton ptit nom ?"
+print "> "
+user_name = gets.chomp
+
+print "bonjour "
+puts user_name
