9e4efd09a25cc54d76ed2983fcd17744fbacc90f
margauxCortier <cortier.margaux@gmail.com>
2018-09-19 17:22:12 +0200

    ajout de l'exo 6

---
+++ exo_06.rb
@@ -0,0 +1,9 @@
+number_of_hours_worked_per_day = 10
+number_of_days_worked_per_week = 5
+number_of_weeks_in_THP = 11
+
+puts "Travail : #{number_of_hours_worked_per_day * number_of_days_worked_per_week * number_of_weeks_in_THP}"
+
+puts "et en minutes ça fait : #{numbers_of_minutes_in_an_hour * number_of_hours_worked_per_day * number_of_hours_worked_per_day * number_of_weeks_in_THP}"
+
+#il se passe que nous n'avons pas enregistré la valeur en nombre de nos min pour exercer ce calcul
