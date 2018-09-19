656ac4447cf375c983c49c333f591dc3a1313855
margauxCortier <cortier.margaux@gmail.com>
2018-09-19 16:09:26 +0200

    troisieme exo

---
+++ exo_03.rb
@@ -0,0 +1,8 @@
+message = "Bonjour monde !
+#{}Et avec une voix sexy ça donne: Bonjour monde !"
+
+puts message
+
+"il ne se passe rien mis à part que nous venons
+à ruby que nous voulions ajouter le contenu d'une variable
+ici vide"
